c334086cc49f6b4bdcc140e25ea566bb3d5ea025
farmanullahQAU <farmanullahk437@gmail.com>
2022-01-21 00:28:13 +0500

    about add

--- lib/views/about/about_view.dart
+++ lib/views/about/about_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
@@ -21,108 +22,129 @@ class AboutView extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-    Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: Container(
-                  height: Get.height,
-                  width: Get.width,
-               
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-               
-                      // RichText(
-                      //   text: TextSpan(
-                      //     text: "As a Computer science graduate i am exicted to begin my career as a software enginerr ",
-                      //     style: TextStyles.body1
-                      //         ?.copyWith(fontWeight: FontWeight.bold),
-                      //     children: <TextSpan>[
-                      //       TextSpan(
-                      //           text: 'Farman Ullah',
-                      //           style: TextStyles.heading5?.copyWith(
-                      //               fontWeight: FontWeight.bold,
-                      //               color: Colors.cyan)),
-                      //     ],
-                      //   ),
-                      // ),
-                    
-                    
-                    
-                      // FittedBox(
-                      //   child: Text(
-                      //     "Projects Completed ",
-                      //     style: TextStyles.heading2?.copyWith(
-                      //         color: Colors.white, fontWeight: FontWeight.bold),
-                      //   ),
-                      // ),
-
-    AnimatedAlign(
-
-         alignment: ResponsiveWrapper.of(context).isSmallerThan(TABLET) ? Alignment.bottomLeft : Alignment.bottomRight,
+           Padding(
+             padding:EdgeInsets.symmetric(vertical: 10,horizontal: 20) ,
+             child: Container(
+               width: double.infinity,
+               child: AnimatedAlign(
+                      alignment: ResponsiveWrapper.of(context).isSmallerThan(TABLET) ? Alignment.center: Alignment.topLeft,
       duration: const Duration(seconds: 1),
       curve: Curves.fastOutSlowIn,
-        child: Row(
-          children: [
-    Flexible(child: FaIcon(FontAwesomeIcons.graduationCap,size: 100,)),
-SizedBox(width: 10,),
-    Flexible(child:  
-      FittedBox(
-      child: RichText(
-                      text: TextSpan(
-                        text: "Msc Computer science ",
-                        style: TextStyles.heading4
-                            ?.copyWith(fontWeight: FontWeight.bold),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: 'Quaid-i- azam university Islamabad',
-                              style: TextStyles.heading4?.copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.cyan)),
-                        ],
-                      ),
-                    ),
-      ),), 
-          ],
-        ),
-      ),
-         AnimatedAlign(
+                 child: Container(
+                   child:FaIcon(FontAwesomeIcons.graduationCap,size: 50,)),
+                 ),
+               ),
+             ),
+           
+               
+          // RichText(
+          //   text: TextSpan(
+          //     text: "As a Computer science graduate i am exicted to begin my career as a software enginerr ",
+          //     style: TextStyles.body1
+          //         ?.copyWith(fontWeight: FontWeight.bold),
+          //     children: <TextSpan>[
+          //       TextSpan(
+          //           text: 'Farman Ullah',
+          //           style: TextStyles.heading5?.copyWith(
+          //               fontWeight: FontWeight.bold,
+          //               color: Colors.cyan)),
+          //     ],
+          //   ),
+          // ),
+        
+        
+        
+          // FittedBox(
+          //   child: Text(
+          //     "Projects Completed ",
+          //     style: TextStyles.heading2?.copyWith(
+          //         color: Colors.white, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
 
-               alignment: ResponsiveWrapper.of(context).isSmallerThan(TABLET) ? Alignment.topLeft : Alignment.center,
-              duration: const Duration(seconds: 1),
-              curve: Curves.fastOutSlowIn,
-           child: SizedBox(
-
-                     
-                        width: Get.width*0.4,
-                        height: Get.width*0.2,
-             
-                        child: Card(
-                          
-                          color: Colors.blue.withOpacity(0.3),
-                          child: Column(
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+ Container(
+               width: double.infinity,
+               child: FittedBox(
+                          child: Text(
+                            "GRADUATION ",
+           
+           
+                            style: 
                             
-                crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                      
-                      
-                      Text("As a Computer science graduate i am exicted to begin my career as a software enginerr",
-                      
-                      textAlign:TextAlign.center,
-                      )
-                      
-                      
-                        ],),),
-                      ),
-         )
-                    ],
-                  ),
-                ),
-              ),
-            
-          
-  
+                            ResponsiveWrapper.of(context).isSmallerThan(TABLET)?
+                            
+                            TextStyles.heading4?.copyWith(
+                                color: Colors.cyan.shade900, fontWeight: FontWeight.bold):
+                                 TextStyles.heading3?.copyWith(
+                                color: Colors.cyan.shade900, fontWeight: FontWeight.bold)
+                          ),
+                        ),
+             ), 
+
+             FittedBox(child: Text("MSC",style: TextStyles.heading1,)),
+              FittedBox(child: Text("COMPUTER SCIENCE",style: TextStyles.heading1,)),
+
+ FittedBox(
+   child: Text(
+     "QUAID-I-AZAM UNIVERSITY ISLAMABAD ",
+             
+             
+     style: TextStyles.heading1?.copyWith(color: Colors.white,fontWeight: FontWeight.bold)
+     
+   
+   ),
+ ),
+
+        ],
+      ),
+
+         Container(
+           width: double.infinity,
+           height: ResponsiveWrapper.of(context).scaledWidth/3,
+           margin: EdgeInsets.zero,
+           
+           color: Colors.cyan.shade900.withOpacity(0.5),
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             crossAxisAlignment: CrossAxisAlignment.center,
+             children: [
+               Text("I AM FARMAN ULLAH, AS A COMPUTER SCIENCE GRADUATE I HAVE STARTED MY CAREER AS A SOFTWARE DEVELOPER, I BUILD  TOP QUALITY APPLICATION FOR ANDROID iOS AND WEB  USING GOOGLE'S FLUTTER,",
+         textAlign:TextAlign.center,
+         
+         style: GoogleFonts.aclonica(  
+         
+         
+         ),
+         ),
+
+         Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           
+           children: [
+
+
+ Flexible(child: Icon(Icons.email,size: 40,),),
+             
+             Flexible(child: FittedBox(child: Text("farmanullahk437@gmail.com",style: TextStyles.heading4,)),),
+
+         ],)
+         
+            ,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Flexible(child: Icon(Icons.phone,size: 40,),),
+             
+             Flexible(child: Text("+923049575366",style: TextStyles.heading4,),)
+            ],)
+             ],
+           ),)
         ],
       ),
     );
