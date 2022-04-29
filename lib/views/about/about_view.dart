59f8f4ed32970f169b76a07baffcad34b9792727
farmanullahQAU <farmanullahk437@gmail.com>
2022-04-29 18:11:40 +0500

    education page added

--- lib/views/about/about_view.dart
+++ lib/views/about/about_view.dart
@@ -17,25 +17,22 @@ class About extends GetView<AboutViewController> {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth=Get.width;
+    final screenWidth = Get.width;
+    final height = Get.height;
+
     return WillPopScope(
-
-         onWillPop: () async {
-
-           Get.find<MenuController>().currentIdex=0;
+      onWillPop: () async {
+        Get.find<MenuController>().currentIdex = 0;
         return true;
       },
       child: Scaffold(
-
         drawer: AppDrawer(),
         key: Get.find<MenuController>().aboutScaffoldKey,
         backgroundColor: Colors.black,
         body: Stack(
           alignment: Alignment.topLeft,
           children: [
-        
-        
-        /*
+            /*
 
 
          ScaleTransition(
@@ -44,265 +41,282 @@ class About extends GetView<AboutViewController> {
            S
             */
 
-            
             SingleChildScrollView(
-              child: Container(
-                height: 700,
-                
+              child: Padding(
+                padding:  EdgeInsets.only(top: height*0.2),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                Expanded(child: Align(
-                  
-                  alignment: Alignment.center,
-                  child: _addAboutContents(screenWidth, context))),
-            
-            
+                    _addAboutContents(screenWidth, context),
+                            SizedBox(height: 150,),
+
+
+                            _addEducationContents(context,screenWidth),
+
+
+
+
                   ],
                 ),
               ),
             ),
 
-            
-        
-        
-        
-        //shows drawer for mobile and menu row for tablet and desktop
-          Header(onTap: (){
-          
-            //open and close drawer for mobile view
-            Get.find<MenuController>().controlAboutMenu();
-          
-          
-          },), 
+            //shows drawer for mobile and menu row for tablet and desktop
+            Header(
+              onTap: () {
+                //open and close drawer for mobile view
+                Get.find<MenuController>().controlAboutMenu();
+              },
+            ),
           ],
         ),
-      
-      
-      
-      
       ),
     );
   }
 
   SizeTransition _addAboutContents(double screenWidth, BuildContext context) {
-    var title = Text(aboutMe.toUpperCase(),style:TextStyles.headingTextStyle);
+    var title = Text(aboutMe.toUpperCase(), style: TextStyles.headingTextStyle);
+    var description = Text(
+      aboutDescription,
+      textAlign: TextAlign.justify,
+      style: TextStyles.subtitle1?.copyWith(color: Colors.white),
+    );
     return SizeTransition(
+      sizeFactor: controller.aboutAnimation,
+      axis: Axis.horizontal,
+      axisAlignment: 0.0,
+      child: ResponsiveWidget(
+        largeScreen: Container(
+          color: Colors.black,
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(500),
+                    bottomLeft: Radius.circular(500))),
+            margin: EdgeInsets.only(
+                left: screenWidth * 0.1, top: 0, bottom: 0, right: 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: AnimatedSize(
+                      curve: Curves.easeIn,
+                      duration: const Duration(seconds: 1),
+                      child: _addImageAvatar(context)),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      title,
+                      SizedBox(
+                        height: 10,
+                      ),
+                      description,
 
-                  sizeFactor: controller.aboutAnimation,
-            axis: Axis.horizontal,
-            axisAlignment: 0.0,
-            child: ResponsiveWidget(
-              largeScreen:
-            
-            
-              Container(
-                color: Colors.black,
-                
-                      child: Container(
-                
-                
-                decoration: BoxDecoration(
- 
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(500),bottomLeft: Radius.circular(500))),
-                margin: EdgeInsets.only(left: screenWidth*0.1,top:0,bottom: 0,right: 0),
-                
-                
-               child: Row(
-                 mainAxisAlignment: MainAxisAlignment.start,
-                 crossAxisAlignment: CrossAxisAlignment.center,
-                
-                
-                
-                
-               children: [
-                
-                
-                
-                 Flexible(
-                   flex: 1,
-                   child: AnimatedSize(
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        width: 200,
+                        height: 5,
+                        color: Colors.red,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      _addSocilaIcon(50),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        //mobile
+        smallScreen: Container(
+          decoration: const BoxDecoration(
+            color: Colors.black,
+
+         
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedSize(
                         curve: Curves.easeIn,
                         duration: const Duration(seconds: 1),
-                        child: _addImageAvatar(context)
-                      ),
-                 ),
-                const SizedBox(width: 20,),
-                  Flexible(
-                    flex: 1,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                
-                       title,
-                       SizedBox(height: 10,),
-                       Text(aboutDescription,textAlign: TextAlign.justify,
-                
-                       style: TextStyles.subtitle1?.copyWith(color: Colors.white),
-                
-                       ),
-                      const SizedBox(height: 30,),
-                        Container(width: 200,height:
-                        5,color: Colors.red,),
-                SizedBox(height: 10,),
-                        _addSocilaIcon(50),
-                
-                      ],
-                    ),
-                  )
-                
-                
-              ,
-                
-                
-                
-               ],
-               ),
-                      ),
-                
-                
-              ),
-            
-            smallScreen:
-            
-            
-               Container(
-                 decoration: const BoxDecoration(
-                   
-                color: Colors.black,
-                   
-                  //  image: DecorationImage(
-                   
-                   
-                  // opacity: 0.3,
-                  // fit: BoxFit.cover,
-                  // image: AssetImage('assets/pic2.jpg'))
-                   
-                   
-                  ),
-                         child: Column(
-                   mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                   
-                   
-                   
+                        child: _addImageAvatar(context)),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
                           SizedBox(
-                   
-               child: Column(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                   
-                   
-                   
-                   
-               children: [
-                   
-                   
-                 AnimatedSize(
-                       curve: Curves.easeIn,
-                       duration: const Duration(seconds: 1),
-                       child: _addImageAvatar(context)
-                     ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(height: 10,),
-                     
-                        title,
-                        SizedBox(height: 10,),
-                        Text(aboutDescription,textAlign: TextAlign.center,
-                     
-                        style: TextStyles.subtitle1?.copyWith(color: Colors.white),
-                     
-                        ),
-                      // SizedBox(height: 30,),
-                      //    Container(width: 200,height:
-                      //    5,color: Colors.red,),
-
-                      SizedBox(height: 20,),
-                     
-                         Align(
-                             alignment: Alignment.center,
-                             child: _addSocilaIcon(30)),
-                     
-                      ],
-                    ),
-                  )
-                   
-                   
-                           ,
-                   
-                   
-                   
-               ],
-               ),
+                            height: 10,
                           ),
-                           ],
-                         ),
-                   
-                   
-                           ),
-            
-            
-            
-            
-            ),
-          );
+
+                          title,
+                          SizedBox(
+                            height: 10,
+                          ),
+                          //add description text
+                          description,
+
+                          SizedBox(
+                            height: 20,
+                          ),
+                             Container(
+                        width: 200,
+                        height: 5,
+                        color: Colors.red,
+                      ),
+
+SizedBox(height: 10,),
+                          _addSocilaIcon(30),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
+  Widget _addImageAvatar(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Colors.white,
+      radius: ResponsiveWidget.isSmallScreen(context) ? 155 : 205,
+      child: CircleAvatar(
+        radius: ResponsiveWidget.isSmallScreen(context) ? 150 : 200,
+        backgroundImage: Image.asset(
+          "assets/pic.jpeg",
+          fit: BoxFit.cover,
+        ).image,
+      ),
+    );
+  }
 
+  Widget _addSocilaIcon(double iconSize) {
+    return Row(
+      //show in start for small screen
 
-  Widget _addImageAvatar(BuildContext context){
-return CircleAvatar(
-backgroundColor: Colors.white,
- radius: ResponsiveWidget.isSmallScreen(context)?155:205,
-  child:   CircleAvatar(
-    radius:ResponsiveWidget.isSmallScreen(context)?150:200,
+      mainAxisAlignment: !ResponsiveWidget.isSmallScreen(Get.context!)
+          ? MainAxisAlignment.start
+          : MainAxisAlignment.center,
+      children: [
+        ButtonBar(
+          children: [
+            FaIcon(
+              FontAwesomeIcons.linkedin,
+              color: socialIconColor,
+              size: iconSize,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            FaIcon(
+              FontAwesomeIcons.twitterSquare,
+              color: socialIconColor,
+              size: iconSize,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            FaIcon(
+              FontAwesomeIcons.mailBulk,
+              color: socialIconColor,
+              size: iconSize,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            FaIcon(
+              FontAwesomeIcons.youtube,
+              color: socialIconColor,
+              size: iconSize,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  _addEducationContents(BuildContext context,double screenWidth) {
+    var title = Text(education.toUpperCase(), style: TextStyles.headingTextStyle);
+
+ final educationDescription = Text(
+      aboutDescription,
+      textAlign: TextAlign.justify,
+      style: TextStyles.subtitle1?.copyWith(color: Colors.white),
+    );
+
+return ResponsiveWidget(largeScreen: Padding(
+  padding:  EdgeInsets.only(left:screenWidth*0.1),
+  child:   Row(
     
-    backgroundImage: Image.asset(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+   Flexible(
 
+     flex: 1,
+     child: Opacity
+     (
+       opacity: 0.0,
+       child: _addImageAvatar(context)),
+   ),
+  
+  Flexible(
+    flex: 1,
+    child: Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+  title,
 
-    "assets/pic.jpeg",fit: BoxFit.cover,).image,
+          educationDescription,
+        ],
+      ))),
+
+   
+  
+  ],),
+),
+
+smallScreen: Container(
+
+  margin: EdgeInsets.symmetric(horizontal: 20),
+  child:   Column(
     
-    
-    ),
+    children: [
+  title,
+SizedBox(height: 20,),
+  educationDescription,
+  
+  
+  
+  
+  ],),
+),
+
 );
 
   }
-
- Widget _addSocilaIcon(double iconSize) {
-
-
-    return Row(
-      
-      
-      mainAxisAlignment:ResponsiveWidget.isLargeScreen(Get.context!)? MainAxisAlignment.start:
-      
-      
-      MainAxisAlignment.center
-      ,
-      children:  [
-    
-    ButtonBar(children: [
-    
-    FaIcon(FontAwesomeIcons.linkedin,color: socialIconColor,size: iconSize,),
-    SizedBox(width: 10,),
-    FaIcon(FontAwesomeIcons.twitterSquare,color: socialIconColor,size: iconSize,),
-    SizedBox(width: 10,),
-
-    FaIcon(FontAwesomeIcons.mailBulk,color: socialIconColor,size: iconSize,),
-    SizedBox(width: 10,),
-
-    FaIcon(FontAwesomeIcons.youtube,color: socialIconColor,size: iconSize,),
-    
-    
-    ],)
-    
-    ],);
-  }
 }
-
- 
